9d2a2b6eb7fb8c37bb73ef104a68b0c3c3b558ac
ainala2610 <ajay.in2610@gmail.com>
2025-08-05 17:35:16 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,36 @@
+description = "The name of the Azure Resource Group"
+  type        = list(string)
+}
+
+variable "username" {
+  description = "The name of the Azure Resource Group"
+  type        = string
+}
+
+
+variable "password" {
+  description = "The name of the Azure Resource Group"
+  type        = string
+  sensitive   = true
+}
+
+variable "publisher" {
+  description = "The name of the Azure Resource Group"
+  type        = string
+}
+
+variable "os" {
+  description = "The name of the Azure Resource Group"
+  type        = string
+}
+
+
+variable "sku" {
+  description = "The name of the Azure Resource Group"
+  type        = string
+}
+
+variable "os-version" {
+  description = "The name of the Azure Resource Group"
+  type        = string
+}
